1185153dd6bcf68c32d02656e9323a9d3e200829
DmitriyPrischep <dmitriygoogle@yandex.ru>
2016-05-22 22:44:42 +0300

    added wgs84_to_lonlat(), init_db() to coordinates, database

--- APP/coordinates.py
+++ APP/coordinates.py
@@ -1,2 +1,19 @@
 from math import fmod, sqrt, atan2, sin, degrees, pi, log, tan
 from utils import jt_to_gmst
+
+def wgs84_to_lonlat(x, y, z, gmst):
+    a = 6378.137
+    b = 6356.7523142
+    r = sqrt(x * x + y * y)
+    f = (a - b) / a
+    e2 = 2 * f - f * f
+
+    lon = atan2(y, x) - gmst
+    lat = atan2(z, r)
+
+    for i in range(0, 20):
+        sl = sin(lat)
+        c = 1 / sqrt(1 - e2 * sl * sl)
+        lat = atan2(z + a * c * e2 * sl, r)
+
+    return lon, lat
--- APP/database.py
+++ APP/database.py
@@ -16,3 +16,39 @@ class DBController:
 
     def __del__(self):
         self.conn.close()
+
+    def init_db(self):
+        self.c.execute(
+            '''create table if not exists sats (
+                   norad       integer not null primary key,
+                   name        text,
+                   intl        text,
+                   country     text,
+                   year        integer
+               ) without rowid'''
+        )
+        self.c.execute(
+            '''create table if not exists orbdata (
+                   norad       integer not null,
+                   date        integer,
+                   epoch_yr    integer,
+                   epoch_day   real,
+                   bstar       real,
+                   inclo       real,
+                   nodeo       real,
+                   ecco        real,
+                   argpo       real,
+                   ibexp       real,
+                   mo          real,
+                   no          real,
+                   primary key (norad, date)
+               ) without rowid'''
+        )
+
+    def get_all(self):
+        sats = []
+        self.c.execute("select * from sats")
+        for rs in self.c.fetchall():
+            sat = self.row_to_sat(rs)
+            sats.append(sat)
+        return sats
