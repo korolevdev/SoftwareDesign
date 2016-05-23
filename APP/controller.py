b5242349905689a9734955a73cc465e91719438d
Владимир Бибин <devbibinva@gmail.com>
2016-05-23 14:11:23 +0300

    event classes added

--- APP/controller.py
+++ APP/controller.py
@@ -4,3 +4,33 @@ class Event:
         self.data = data
     def process(self, core):
         pass
+
+class EvRender(Event):
+
+    def process(self, core):
+        time_a = self.data[0]
+        time_b = self.data[1]
+        sats = []
+        for n in self.data[2]:
+            orbs = core.dbc.get_orbits_between(n, time_a, time_b)
+            lines = core.rdr.render(time_a, time_b, orbs)
+            sats.append((n, lines))
+        core.gui.render_sats(sats, time_a, time_b)
+
+
+class EvReloadAll(Event):
+
+    def process(self, core):
+        core.reload_sats()
+
+
+class EvSaveTLE(Event):
+
+    def process(self, core):
+        core.save_tle(self.data[0], self.data[1])
+
+
+class EvLoadTLE(Event):
+
+    def process(self, core):
+        core.load_tle(self.data)
