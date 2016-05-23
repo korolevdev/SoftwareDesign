24cff6222326336923f32184ba763917d6b42601
Владимир Бибин <devbibinva@gmail.com>
2016-05-23 12:22:16 +0300

    error classes added

--- APP/decoder.py
+++ APP/decoder.py
@@ -1,6 +1,37 @@
 from sgp import Sat
 
 
+class DecoderError(Exception):
+
+    def __init__(self, err):
+        self.value = err
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class NoDecoderError(DecoderError):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "no suitable decoder for " + repr(self.value)
+
+
+class EncodedValueError(DecoderError):
+
+    def __init__(self, fmt, err, pos=None):
+        self.value = fmt
+        self.err = err
+        self.pos = pos
+
+    def __str__(self):
+        if self.pos is not None:
+            return repr(self.value) + ": " + self.err + " at " + str(self.pos)
+        else:
+            return repr(self.value) + ": " + self.err
+
 class Decoder:
 
     def __init__(self, decoders=[]):
